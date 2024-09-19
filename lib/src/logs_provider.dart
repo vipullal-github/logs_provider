b3787aa5ae03eb100236819dbbbbfc6acc03fb76
Vipul Lal <vipullal100@gmail.com>
2024-09-19 09:34:45 +0800

    Working copy

--- lib/src/logs_provider.dart
+++ lib/src/logs_provider.dart
@@ -3,11 +3,16 @@ import 'dart:isolate';
 
 import 'package:flutter/widgets.dart';
 
+/*
+  LogsProvider widget is an InheritedWidget which can be placed into the widget 
+  tree.
+*/
 class LogsProvider extends InheritedWidget {
   final List<String> _messages = List.empty(growable: true);
   UnmodifiableListView<String> get messages => UnmodifiableListView(_messages);
   int get messageCount => _messages.length;
   final ReceivePort _recvPort = ReceivePort("LogsProvider");
+  SendPort get sendPort => _recvPort.sendPort;
 
   LogsProvider({super.key, required super.child}) {
     _recvPort.listen((message) => addMessage(message));
